1f7d7aeeff51ec2b55faca31ae82d64c50e8342e
Stephen Bayer <sabayer@gmail.com>
2018-08-10 15:48:48 -0700

    working with components

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  templateUrl: './events-list.component.html'
+})
+export class EventsListComponent {
+  event = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '09/26/2016',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      addresss: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  };
+}
